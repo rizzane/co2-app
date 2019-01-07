7f2eac4ed8cb36f062176cd40d9cacbc559aa061
rizzane <rissanen.s.joonas@gmail.com>
2019-01-07 17:18:18 +0200

    heroku

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ var express = require('express'),
     port = process.env.PORT || 3001,
     bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require('./api/routes/routes'); //importing routes
 routes(app); //register the route
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+    app.use(‘/’, express.static(`${__dirname}/client/public`));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "client/public", "index.html"));
     });
-}
-app.listen(port);
-
-console.log("RESTful API server started on: " + port);
+// }
+app.listen(port, () => console.log("Listening on port ${port}"));
