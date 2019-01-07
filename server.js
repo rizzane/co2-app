f905fd986b1f9653a45b7a0b8e4dda75bea7561d
rizzane <rissanen.s.joonas@gmail.com>
2019-01-07 17:53:56 +0200

    heroku

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require('./api/routes/routes'); //importing routes
 routes(app); //register the route
 
-app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"))
