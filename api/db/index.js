8c0f7dc2bdcfa7966f394d64cdc8d473bf043b04
rizzane <rissanen.s.joonas@gmail.com>
2019-01-13 21:16:13 +0200

    added attribute for db connection

--- api/db/index.js
+++ api/db/index.js
@@ -6,7 +6,8 @@ dotenv.config();
 
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
 });
 
 pool.on('connect', () => {
