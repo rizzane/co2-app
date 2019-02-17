243aba569bcb968a3bbb774e3926ea9356cffb39
rizzane <rissanen.s.joonas@gmail.com>
2019-02-17 23:37:44 +0200

    small fixes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const data = require("./api/data");
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const path = require("path");
@@ -10,7 +9,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var routes = require('./api/routes/routes'); //importing routes
+var routes = require('./routes/routes'); //importing routes
 routes(app); //register the route
 
 // Anything that doesn't match the above, send back index.html
